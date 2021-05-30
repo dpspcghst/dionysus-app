ac516df76474c7b11db282524045a1b2f7df47f2
Hemingway <ghostblac@protonmail.com>
2021-05-29 22:09:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # dionysus
 
+import random
+import string
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,8 +13,24 @@ def index():
     """
     """
 
-    return "Congratulations, it's a web app!"
+    return "<h1>Dionysus</h1>"
+
+
+@app.route("/<int:length>")
+def random_password(length):
+    """
+    """
+
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    numbers = "123456789"
+    symbols = "[]{}()*;/,._-"
+    characters = lower + upper + numbers + symbols
+    password_length = length
+    password = str("".join(random.sample(characters, password_length)))
+    return password
 
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
+
